8795e62da6c0c049a99009320a9e94f84787d081
wubet <wube.tuffa@gmail.com>
2020-01-12 14:27:18 -0800

    Initial commit

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace TraficMonitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trafic trafic = new Trafic();
+            CountManager _countMgr = new CountManager(trafic);
+            _countMgr.StartTrafic();
+            _countMgr.requestTrafic();
+            Console.ReadLine();
+
+        }
+    }
+
+    public class CountManager
+    {
+        private ITrafic _trafic;
+        public CountManager(ITrafic trafic)
+        {
+            _trafic = trafic;
+        }
+        public void StartTrafic()
+        {
+            Timer myTimer = new Timer();
+            myTimer.Elapsed += new ElapsedEventHandler(HitTrafic);
+            myTimer.Interval = 300; // 1000 ms is one second
+            myTimer.Start();
+
+        }
+
+        public void requestTrafic()
+        {
+            Timer myTimer = new Timer();
+            myTimer.Elapsed += new ElapsedEventHandler(RequestTrafic);
+            myTimer.Interval = 600; // 1000 ms is one second
+            myTimer.Start();
+
+        }
+
+        private void HitTrafic(object source, ElapsedEventArgs e)
+        {
+            _trafic.Hit();
+            Console.WriteLine("Trafic Hit " + DateTime.Now);
+        }
+
+        private void RequestTrafic(object source, ElapsedEventArgs e)
+        {
+            int result = _trafic.HitCount();
+            Console.WriteLine("Trafic Request " + DateTime.Now + " " + result);
+        }
+
+
+    }
+}
---
+++ ConsoleApp13/Trafic.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace TraficMonitor
+{
+    public class Trafic : ITrafic
+    {
+        private int _count;
+        private int _totalCount = 0;
+        private List<Tuple<DateTime, int>> _listCount;
+
+        public Trafic()
+        {
+            _listCount = new List<Tuple<DateTime, int>>();
+            Start();
+        }
+
+        public void Hit()
+        {
+            _count++;
+        }
+        public int HitCount()
+        {
+            DateTime current = DateTime.Now;
+            for(int i = _listCount.Count-1; i > 0; --i)
+            {
+                var hitCount = _listCount[i];
+                if((current - hitCount.Item1).TotalSeconds <= 1)
+                {
+                    _totalCount += hitCount.Item2;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return _totalCount;
+        }
+
+        private void Start()
+        {
+            Timer myTimer = new Timer();
+            myTimer.Elapsed += new ElapsedEventHandler(CountHandler);
+            myTimer.Interval = 1000; // 1000 ms is one second
+            myTimer.Start();
+
+        }
+
+        private void CountHandler(object source, ElapsedEventArgs e)
+        {
+            if(_count > 0)
+            {
+                var tuple = new Tuple<DateTime, int>(DateTime.Now, _count );
+                _listCount.Add(tuple);
+                _count = 0;
+                _totalCount = 0;
+            }
+        }
+    }
+}
